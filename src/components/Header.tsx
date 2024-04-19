d20527c764e9bfb1e033f89a6a8b6eb0c02854cd
Sakura Tanaka <sito6496t@gmail.com>
2024-04-19 23:25:47 +0900

    refactore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
-import HomeLink from "@/app/HOmeLink";
-import { sans } from "@/app/fonts";
-import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { metadata } from "@/app/layout";
-import { siteConfig } from "@/app/site";
+import HomeLink from '@/components/HomeLink';
+import { sans } from '@/app/styles/fonts';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { metadata } from '@/app/layout';
+import { siteConfig } from '@/app/site';
 
 export const Header = () => {
   return (
     <div className="font-bold text-gray-100 flex justify-between max-w-[1500px] mx-auto gap-28 items-end py-6">
       <HomeLink />
       <ul className="text-base flex items-end gap-6">
-        <li className={[`uppercase hover:text-gray-50`, sans.className].join(" ")}>
+        <li className={[`uppercase hover:text-gray-50`, sans.className].join(' ')}>
           <Link href="#">About</Link>
         </li>
         <li>
